795fcf98bb73665810116cd3ed7b49a8e3ea249f
Reboish <reara7720@oslomet.no>
2026-02-27 15:49:33 +0100

    Add SQL tables for station, rental, and customer

--- init-scripts/01-init-database.sql
+++ init-scripts/01-init-database.sql
@@ -5,6 +5,42 @@
 
 -- Opprett grunnleggende tabeller
 
+CREATE TABLE stasjon (
+  stasjon_id BIGSERIAL PRIMARY KEY,
+  navn TEXT NOT NULL,
+  adresse TEXT NOT NULL
+);
+
+CREATE TABLE laas(
+  laas_id BIGSERIAL PRIMARY KEY,
+  stasjon_id BIGINT NOT NULL REFERENCES stasjon(staasjon_id)
+);
+
+CREATE TABLE sykkel(
+  sykkel_id BIGSERIAL PRIMARY KEY,
+  laas_id BIGINT REFERENCES laas(laas_id) -- NULL = utleid
+);
+
+CREATE TABLE kunde(
+  kunde_id BIGSERIAL PRIMARY KEY,
+  navn TEXT NOT NULL,
+  epost TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE utleie (
+  utleie_id BIGSERIAL PRIMARY KEY,
+    kunde_id BIGINT NOT NULL REFERENCES kunde(kunde_id),
+    sykkel_id BIGINT NOT NULL REFERENCES sykkel(sykkel_id),
+    utlevert_tid TIMESTAMPTZ NOT NULL,
+    innlevert_tid TIMESTAMPTZ, -- NULL = aktiv
+    leiebelop NUMERIC(10,2) NOT NULL,
+    fra_laas_id BIGINT NOT NULL REFERENCES laas(laas_id),
+    til_laas_id BIGINT REFERENCES laas(laas_id) -- NULL til innlevering
+);
+  
+
+
+
 
 
 -- Sett inn testdata
